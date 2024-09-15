0e92f737f33e2c30f475ade438a6815bef732bba
Hamed Naghdi <hamed.naghdi@gmail.com>
2024-09-15 22:47:40 +0200

    add spartan (before adding sample btn)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,12 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: 'admin',
+    loadChildren: () => import('./pages/admin/admin.module').then((m) => m.AdminModule),
+    title: 'Dashboard | Cimdata',
+    data: { preload: true },
+  },
+  {
     path: '',
     loadChildren: () => import('./pages/home/home.module').then((m) => m.HomeModule),
     // title: 'Home | Cimdata',
