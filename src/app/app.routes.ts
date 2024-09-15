909adc30386ebee6e2539689227601a11ba9b076
Hamed Naghdi <hamed.naghdi@gmail.com>
2024-09-15 19:28:21 +0200

    add home page module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./pages/home/home.module').then((m) => m.HomeModule),
+    // title: 'Home | Cimdata',
+    data: { preload: true },
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
