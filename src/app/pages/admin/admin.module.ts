32a74df76cbd963930fe6838f297a5c268facd7a
Hamed Naghdi <hamed.naghdi@gmail.com>
2024-09-15 23:48:27 +0200

    spartan is almost ready, just speration of admin remains

--- src/app/pages/admin/admin.module.ts
+++ src/app/pages/admin/admin.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 
 @NgModule({
   declarations: [AdminComponent],
-  imports: [CommonModule, AdminRoutingModule],
+  imports: [CommonModule, AdminRoutingModule, HlmButtonDirective],
+  exports: [HlmButtonDirective],
 })
 export class AdminModule {}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@
 module.exports = {
   presets: [require('@spartan-ng/ui-core/hlm-tailwind-preset')],
   content: [
-    './src/app/pages/admin/**/*.{html,ts}',
+    './src/app/pages/admin/**/*.{html,ts,scss}',
+    "./libs/ui/**/*.{html,ts}",
   ],
   theme: {
     extend: {},
