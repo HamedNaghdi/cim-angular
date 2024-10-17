de238daa21456bc0d618f9c40f59d1ea55512c4c
Hamed Naghdi <hamed.naghdi@gmail.com>
2024-10-17 10:11:47 +0200

    configure font/variable-font

--- src/app/layouts/admin-layout/admin-layout.component.ts
+++ src/app/layouts/admin-layout/admin-layout.component.ts
@@ -8,13 +8,17 @@ import { NavComponent } from './nav/nav.component';
   standalone: true,
   imports: [RouterOutlet, NavComponent],
   templateUrl: './admin-layout.component.html',
-  styleUrl: './admin-layout.component.scss',
+  styleUrls: [
+    './admin-shadcn-tailwind.scss',
+    './admin-fonts.scss',
+    './admin-layout.component.scss',
+  ],
   encapsulation: ViewEncapsulation.None,
 })
 export class AdminLayoutComponent implements OnInit {
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
-    this.router.events.subscribe(event => console.log(event))
+    this.router.events.subscribe((event) => console.log(event));
   }
 }
