796887b0de31494626af8ad79f8b8e88e0613971
Hamed Naghdi <hamed-naghdi@storming-studios.com>
2024-10-22 10:38:20 +0200

    add static template for working on real data after

--- src/app/pages/admin/pages/edit/edit.component.ts
+++ src/app/pages/admin/pages/edit/edit.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-edit',
-  standalone: true,
-  imports: [],
+  standalone: false,
+  // imports: [],
   templateUrl: './edit.component.html',
   styleUrl: './edit.component.scss'
 })
--- src/app/pages/admin/pages/list/list.component.ts
+++ src/app/pages/admin/pages/list/list.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-list',
-  standalone: true,
-  imports: [],
+  standalone: false,
+  // imports: [],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export class ListComponent {
-
+  pages = [
+    { id: 1, title: `Page 1` },
+    { id: 2, title: `Page 2` },
+  ]
 }
