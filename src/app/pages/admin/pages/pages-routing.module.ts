42f5658270b625b22b4be820cf401175bdd8cd64
Hamed Naghdi <hamed.naghdi@gmail.com>
2024-10-29 22:15:10 +0100

    add list component for pages in admin panel

--- src/app/pages/admin/pages/pages-routing.module.ts
+++ src/app/pages/admin/pages/pages-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'list',
+    pathMatch: 'full',
+  },
+  {
+    path: 'list',
+    component: ListComponent,
+    pathMatch: 'full',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
