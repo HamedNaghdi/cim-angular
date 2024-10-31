f0a8ecd207dab93497c1cb8bc799fa881083c9b9
Hamed Naghdi <hamed.naghdi@gmail.com>
2024-10-31 11:13:02 +0100

    prepare for api interceptor

---
+++ src/app/core/interceptors/api/api.interceptor.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+
+@Injectable()
+export class ApiInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const authReq = req.clone({
+      // setHeaders: { Authorization: `Bearer YOUR_TOKEN_HERE` }
+    });
+
+    return next.handle(authReq).pipe(
+      tap(event => {
+        console.log('Request:', req);
+      }),
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 0) {
+          console.error('CORS error:', error.message);
+        } else {
+          console.error('Error:', error);
+        }
+        let errorMsg = '';
+        if (error.error instanceof ErrorEvent) {
+          errorMsg = `Error: ${error.error.message}`;
+        } else {
+          errorMsg = `Error Code: ${error.status}\nMessage: ${error.message}`;
+        }
+        return throwError(errorMsg);
+      })
+    );
+  }
+}
+
--- src/app/pages/admin/admin.module.ts
+++ src/app/pages/admin/admin.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { ApiInterceptor } from '../../core/interceptors/api/api.interceptor';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule, AdminRoutingModule, HttpClientModule],
   exports: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: ApiInterceptor, multi: true },
+  ],
 })
 export class AdminModule {}
